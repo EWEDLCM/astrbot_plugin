b517188fb16e368e82e68820a1d62601003e6204
EWEDLCM <168160898+EWEDLCM@users.noreply.github.com>
2025-04-11 11:40:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from astrbot.api.star import Context, Star, register
 import astrbot.api
 
 # --- 插件介绍代码内容 ---
-@register("astrbot_plugin_mp_ewedl", "EWEDL", "MP调用及消息转发", "1.3.7")
+@register("astrbot_plugin_mp", "EWEDL", "MP调用及消息转发", "1.3.7")
 class MediaSearchPlugin(Star):
     """集成媒体搜索、订阅管理以及基于HTTP的分类消息通知功能。
     支持持久化订阅和文件日志。"""
